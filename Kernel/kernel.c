63d3efa31cb5219d42e5c6d3f27c86d03253f53b
nclozza <nclozza@itba.edu.ar>
2017-06-01 16:00:17 -0300

    Changes in types

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -8,14 +8,14 @@
 #include "include/types.h"
 #include "include/system_calls.h"
 
-extern uint8_t text;
-extern uint8_t rodata;
-extern uint8_t data;
-extern uint8_t bss;
-extern uint8_t endOfKernelBinary;
-extern uint8_t endOfKernel;
+extern byte text;
+extern byte rodata;
+extern byte data;
+extern byte bss;
+extern byte endOfKernelBinary;
+extern byte endOfKernel;
 
-static const uint64_t PageSize = 0x1000;
+static const qword PageSize = 0x1000;
 extern unsigned int readk();
 
 static void * const sampleCodeModuleAddress = (void*)0x400000;
@@ -23,15 +23,15 @@ static void * const sampleDataModuleAddress = (void*)0x500000;
 
 typedef int (*EntryPoint)();
 
-void clearBSS(void * bssAddress, uint64_t bssSize) {
+void clearBSS(void * bssAddress, qword bssSize) {
 	memset(bssAddress, 0, bssSize);
 }
 
 void * getStackBase() {
 	return (void*)(
-		(uint64_t)&endOfKernel
+		(qword)&endOfKernel
 		+ PageSize * 8				//The size of the stack itself, 32KiB
-		- sizeof(uint64_t)			//Begin at the top of the stack
+		- sizeof(qword)			//Begin at the top of the stack
 	);
 }
 
